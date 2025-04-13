658057e231436b9484d65d4a27c044cfb2718ef7
destriautami <87311766+destriautami@users.noreply.github.com>
2025-04-14 00:26:42 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -15,21 +15,24 @@ if shape == 1:
     h = int(input('Height :'))
     b = int(input('Base :'))
     area = (h*b)/2
+    print('The area is', area)
 elif shape == 2:
     l = int(input('Length :'))
     w = int(input('Width :'))
     area = l * w 
+    print('The area is', area)
 elif shape == 3: 
     s = int(input('Side :'))
     area = s ** 2
+    print('The area is', area)
 elif shape == 4:
     pi = 3.14
     r = int(input('Radius :'))
     area = pi * (r ** 2)
+    print('The area is', area)
 elif shape == 5:
     print('Quit')
 else:
     print('Something is wrong!')
 
 
-print('The area is', area)
