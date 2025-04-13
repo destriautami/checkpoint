62295929788ad73c07191f26ceb9ca15cd917d0d
destriautami <87311766+destriautami@users.noreply.github.com>
2025-04-14 00:08:57 +0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,35 @@
+print('==================')
+print('Area Calculator 📐')
+print('==================')
+
+print('1) Triangle')
+print('2) Rectangle')
+print('3) Square')
+print('4) Circle')
+print('5) Quit')
+
+shape = int(input('Which shape:'))
+
+
+if shape == 1: 
+    h = int(input('Height :'))
+    b = int(input('Base :'))
+    area = (h*b)/2
+elif shape == 2:
+    l = int(input('Length :'))
+    w = int(input('Width :'))
+    area = l * w 
+elif shape == 3: 
+    s = int(input('Side :'))
+    area = s ** 2
+elif shape == 4:
+    pi = 3.14
+    r = int(input('Radius :'))
+    area = pi * (r ** 2)
+elif shape == 5:
+    print('Quit')
+else:
+    print('Something is wrong!')
+
+
+print('The area is', area)
